daa199bd4633582d5e5b76906b27e2852b6a742f
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 17:23:02 +0300

    Добавьте файлы проекта.

---
+++ zadachiPP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class AgeCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите вашу дату рождения в формате (гггг-мм-дд):");
+        string input = Console.ReadLine();
+        DateTime birthDate;
+        if (DateTime.TryParse(input, out birthDate))
+        {
+            TimeSpan age = DateTime.Now - birthDate;
+            int years = (int)(age.Days / 365.25);
+            int months = (int)((age.Days % 365.25) / 30.44);
+            int days = (int)(age.Days % 30.44);
+
+            Console.WriteLine($"Ваш возраст: {years} лет, {months} месяцев, {days} дней");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный формат даты. Пожалуйста, используйте формат (гггг-мм-дд).");
+        }
+    }
+}
